335ced40e36bd0066246a3354d5061c8bc25f2ea
Jonathan Yu <oninuva@gmail.com>
2017-01-14 18:42:14 -0500

    Added additional helper function to Board

--- Board.cs
+++ Board.cs
@@ -13,4 +13,29 @@ public class Board{
 	Entity GetAt(int x, int y, LayerType type){
 		return tiles[y,x].layers[type];
 	}
+	
+	Tile GetAt(int x, int y){
+		return tiles[y,x];
+	}
+	
+	List<Tile> GetNeighbors(int x, int y){
+		List<Tile> neighbors = new List<Tile>();
+		if(isValid(x + 1, y))
+			neighbors.Add(GetAt(x + 1, y));
+		if(isValid(x, y - 1))
+			neighbors.Add(GetAt(x, y - 1));
+		if(isValid(x - 1, y))
+			neighbors.Add(GetAt(x - 1, y));
+		if(isValid(x, y + 1))
+			neighbors.Add(GetAt(x, y + 1));
+		
+		return neighbors;
+		
+	}
+	
+	bool isValid(int x, int y){
+		if(x < 0 || x > width - 1 || y < 0 || y > height -1)
+			return false;
+		return true;
+	}
 }
